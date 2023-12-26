1d0529ad63490ef39c1d669e62e772ad64c324ba
roman.lebedenko <roman.lebedenko@engelvoelkers.com>
2023-12-26 15:04:55 +0100

    simple email pattern

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,6 @@
-function validateEmail() {}
+function validateEmail(email) {
+    const regex = /[\w]{1,}[@][a-z]{1,}[.][a-z]{2,}$/gm;
+    return regex.test(email);
+}
 
 module.exports = validateEmail;
