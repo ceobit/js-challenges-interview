6469d95ab152205c632141098b350d2f7e72d743
roman.lebedenko <roman.lebedenko@engelvoelkers.com>
2023-12-26 13:49:09 +0100

    phone number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+    // formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // => "(123) 456-7890"
+    // formatPhoneNumber([5, 1, 9, 5, 5, 5, 4, 4, 6, 8]); // => "(519) 555-4468"
+    // formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7]); // => "(345) 501-2527"
+    return `(${arr.slice(0, 3).join('')}) ${arr.slice(3, 6).join('')}-${arr.slice(6, 10).join('')}`
+}
 
 module.exports = formatPhoneNumber;
