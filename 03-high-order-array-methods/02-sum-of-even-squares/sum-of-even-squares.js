ec6c5478d602518976f6f9477e4602b2b24518b3
roman.lebedenko <roman.lebedenko@engelvoelkers.com>
2023-12-29 17:32:33 +0100

    calculateTotalSalesWithTax, sum of even

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,12 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    // sumOfEvenSquares([1, 2, 3, 4, 5]); // 20 (2^2 + 4^2)
+    // sumOfEvenSquares([-1, 0, 1, 2, 3, 4]); // 20 (0^2 + 2^2 + 4^2)
+    // sumOfEvenSquares([]); // 0
+
+    const filterArr = arr.filter(el => el % 2 === 0);
+    const squares = filterArr.map(el => el * el);
+    return  squares.reduce((acc, cur) => acc + cur, 0)
+
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,22 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arr, taxRate) {
+    // calculateTotalSalesWithTax(
+    //     [
+    //         { name: 'Apple', price: 0.5, quantity: 10 },
+    //         { name: 'Banana', price: 0.3, quantity: 20 },
+    //         { name: 'Orange', price: 0.6, quantity: 15 },
+    //     ],
+    //     8
+    // ); // 21.6 (20 + 8% tax)
+
+    // const arr1 = arr.map(el => el.price * el.quantity);
+
+
+    const num = arr.reduce((acc, cur) =>
+        (acc + cur.price * cur.quantity)
+    , 0);
+
+    return num + num * taxRate / 100;
+
+}
 
 module.exports = calculateTotalSalesWithTax;
